9cdbd5ade0ca771f6ba6faec3decfed075e33bea
levchenki <danila.levchenki@gmail.com>
2024-03-24 20:22:17 +0300

    chore: the name of the deploy script has been changed, the network has been changed

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,22 +1,26 @@
 import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomicfoundation/hardhat-verify";
 
 import dotenv from "dotenv";
 
 dotenv.config({path: '../.env'})
 
-const mumbaiUrl = `https://polygon-mumbai.infura.io/v3/${process.env.VITE_INFURA_API_KEY}`;
+const sepoliaUrl = `https://sepolia.infura.io/v3/${process.env.VITE_INFURA_API_KEY}`;
 
 const config: HardhatUserConfig = {
     solidity: "0.8.24",
     networks: {
-        mumbai: {
-            url: mumbaiUrl,
+        sepolia: {
+            url: sepoliaUrl,
             accounts: [process.env.PRIVATE_KEY ?? ""]
         }
     },
     etherscan: {
-        apiKey: process.env.POLYGONSCAN_API_KEY
+        apiKey: process.env.ETHERSCAN_API_KEY
+    },
+    sourcify: {
+        enabled: true
     }
 };
 
--- contracts/scripts/deployERC20ABCContinuousToken.ts
+++ contracts/scripts/deployERC20ABCContinuousToken.ts
@@ -7,19 +7,22 @@ async function main() {
         `Deploying contracts with the account: ${deployer.account.address}`,
     );
 
-    const initialName = 'TestContinuousToken';
-    const initialSymbol = 'TCT';
+    const initialName = 'TestABCToken';
+    const initialSymbol = 'TAT';
     const initialSupply = 10000n * BigInt(1e18);
     const reserveRatio = 900_000;
     const accumulationDuration = 1n;
+    const reserveTokenAddress = process.env.RESERVE_TOKEN_CONTRACT_ADDRESS as `0x${string}`
 
-    const token = await hre.viem.deployContract("AugmentedContinuousToken", [
+
+    const token = await hre.viem.deployContract("ERC20ABCContinuousToken", [
         initialName,
         initialSymbol,
         initialSupply,
         reserveRatio,
         deployer.account.address,
-        accumulationDuration
+        accumulationDuration,
+        reserveTokenAddress
     ], {walletClient: deployer});
 
     console.log("AugmentedContinuousToken deployed to:", token.address);
